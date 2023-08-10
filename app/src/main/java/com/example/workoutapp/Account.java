cf3b5843e4be5a013f5fe358f388c590502800d0
RaillahAdjia <adjiarailla@gmail.com>
2023-08-10 01:04:26 -0500

    Tien Vo and Raillah( we added RegisterActivity.java and registration.xml and we modified login.xml and MainActivity.java)

--- app/src/main/java/com/example/workoutapp/Account.java
+++ app/src/main/java/com/example/workoutapp/Account.java
@@ -1,40 +1,73 @@
 package com.example.workoutapp;
 
-import android.content.res.AssetManager;
 //this is a test push
 public class Account {
 
     private int id;
     private String name;
-    private String email;
+    private double weight;
+    private int Goal;
+    private String dateOfBirth;
+    private int heightFeet;
+    private int heightInches;
 
-    public Account(int id, String name, String email) {
+
+    public Account(int id, String name, double weight, int Goal, String dateOfBirth, int heightFeet, int heightInches) {
         this.id = id;
         this.name = name;
-        this.email = email;
+        this.weight = weight;
+        this.Goal = Goal;
+        this.dateOfBirth = dateOfBirth;
+        this.heightFeet = heightFeet;
+        this.heightInches = heightInches;
+
     }
 
     private void setId(int id) {
         this.id = id;
     }
+    public int getId() { return id; }
 
     public void setName(String name) {
         this.name = name;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public String getEmail() {
-        return email;
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getGoal() { return Goal; }
+
+    public void setGoal(int goal) { Goal = goal; }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getHeightFeet() {
+        return heightFeet;
+    }
+
+    public void setHeightFeet(int heightFeet) {
+        this.heightFeet = heightFeet;
+    }
+
+    public int getHeightInches() {
+        return heightInches;
+    }
+
+    public void setHeightInches(int heightInches) {
+        this.heightInches = heightInches;
     }
 }
--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -1,69 +1,94 @@
 package com.example.workoutapp;
+
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MainActivity extends ComponentActivity {
-    private Button button;
-    private AssetManager assets;
-
+    //private Button button;
+    //private String usernameTest = "test";
+    //private String passTest = "test";
+    //private AssetManager assets;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login2);
-        assets = getAssets();
-        //Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
-        setUpButtons();
+        setContentView(R.layout.login);
+        //assets = getAssets();
+        //Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show();
+        setupButton();
     }
-    private void setUpButtons() {
-        button = (Button) findViewById(R.id.login);
 
-        button.setOnClickListener(new View.OnClickListener() {
+    private void setupButton() {
+        Button login = (Button) findViewById(R.id.login);
+        Button register = (Button) findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                EditText uText = (EditText) findViewById(R.id.userNameString);
-                EditText pText = (EditText) findViewById(R.id.passwordString);
-                int id = authenticate(uText.getText().toString(), pText.getText().toString());
-
-                if(id > 0) {
+                EditText uText = (EditText) findViewById(R.id.inputName);
+                EditText pText = (EditText) findViewById(R.id.inputPassword);
+                int id = authenticate(uText.getText().toString(),pText.getText().toString());
+                if( id > 0 ) {
                     Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                     intent.putExtra("id",id);
                     startActivity(intent);
-                } else {
+                }
+                else {
                     uText.setText("");
                     pText.setText("");
                     uText.setError("Incorrect username and password combination.");
                     pText.setError("Incorrect username and password combination.");
+
                 }
             }
         });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
     }
+
     private int authenticate(String username, String password) {
-        Scanner s;
+        Scanner scan;
         String str = "";
-        String[] creds = null;
+        String[] arr = null;
         int id = -1;
+        File f = new File(getFilesDir().getAbsoluteFile() + "/login.txt");
 
         try {
-            s = new Scanner(assets.open("login.txt"));
-            while(s.hasNext()) {
-                str = s.nextLine();
-                creds = str.split(",");
-                if(username.equalsIgnoreCase(creds[1]) && password.equals(creds[2])) {
-                    id = Integer.parseInt(creds[0]);
-                    break;
+            if(f.exists()) {
+                scan = new Scanner(openFileInput("login.txt"));
+                while (scan.hasNextLine()) {
+                    str = scan.nextLine();
+                    arr = str.split(",");
+                    if (username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])) {
+                        id = Integer.parseInt(arr[0]);
+                        break;
+                    }
+
                 }
+                scan.close();
             }
-            s.close();
-        } catch (IOException e) {
+        }
+        catch(IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
